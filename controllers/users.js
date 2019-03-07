1a3676269ea2dc91aaad74b8afbd40900c619a86
webpagepro <samuel@webpagepro.local>
2019-03-07 14:33:23 -0700

    user crud

--- controllers/users.js
+++ controllers/users.js
@@ -1,50 +1,32 @@
 const knex = require("../db/knex.js");
 
 module.exports = {
-    index: function(req, res) {
-        knex('users')
-        .then((persons) => {
-          res.render('index', {'persons': persons});
-      
+  index: function (req, res) {
+    knex('users')
+      .then((persons) => {
+        res.render('index', { 'persons': persons });
+
         console.log(persons);
-        })
-      },
-  
+      })
+  },
+
   getOne(req, res) {
     knex('users')
       .where('id', req.params.id)
       .then(user => res.json(user))
   },
-addOne(req, res) {
+  addOne(req, res) {
     knex('users')
       .insert(req.body)
       .returning('*')
       .then(newuser => res.json(newUser))
-  } /* ,
+  },
   updateOne(req, res) {
     knex('users')
       .where('id', req.params.id)
       .update(req.body)
       .returning('*')
       .then(updatedUser => res.json(updatedUser))
-  },
-  addToCart(req, res) {
-    knex('users')
-      .where('id', req.params.id)
-      .update({
-        inCart: true
-      })
-      .returning('*')
-      .then(updatedUser => res.json(updatedUser))
-  },
-  removeFromCart(req, res) {
-    knex('users')
-      .where('id', req.params.id)
-      .update({
-        inCart: false
-      })
-      .returning('*')
-      .then(updateduser => res.json(updateduser))
   },
   deleteuser(req, res) {
     knex('users')
@@ -53,5 +35,5 @@ addOne(req, res) {
       .returning('*')
       .then(deletedUser => res.json(deletedUser))
   }
-  */
+
 }
